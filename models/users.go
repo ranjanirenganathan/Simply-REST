afbd661549850e57a35d04a98548b687c5892bc1
Ranjani Renganathan <ranjanivsp@gmail.com>
2018-02-23 14:41:40 -0800

    Update users.go

--- models/users.go
+++ models/users.go
@@ -6,8 +6,8 @@ type User struct {
 	DocumentBase `json:",inline" bson:",inline"`
 	Name    	    string   		`json:"name"  bson:"name"`
 	Status  	    string   		`json:"status"  bson:"status"`
-	Description     string   		`json:"description"  bson:"description"`
-	Address         Address 		`json:"address" bson:"address"`
+	Description         string   		`json:"description"  bson:"description"`
+	Address             Address 		`json:"address" bson:"address"`
 }
 
 
@@ -16,11 +16,11 @@ type Address struct {
 	Email    	    string   		`json:"email"   bson:"email"`
 	Phone   	    string   		`json:"phone"   bson:"phone"`
 	Fax   		    string   		`json:"fax"     bson:"fax"`
-	Address 	    string 		    `json:"address" bson:"address"`
-	City   		    string 			`json:"city"    bson:"city"`
-	State           string		    `json:"state"   bson:"state"`
-	PostalCode   	string    	    `json:"postalCode" bson:"postalCode"`
-	Country		    string			`json:"country" bson:"country"`
+	Address 	    string 		`json:"address" bson:"address"`
+	City   		    string 	        `json:"city"    bson:"city"`
+	State               string		`json:"state"   bson:"state"`
+	PostalCode   	    string    	        `json:"postalCode" bson:"postalCode"`
+	Country		    string		`json:"country" bson:"country"`
 }
 
 
