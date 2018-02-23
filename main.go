c6c8a4c1d1161f906aae51cd46def6de8db8558a
ranjani <ranjanivsp@gmail.com>
2018-02-23 22:14:25 +0000

    main.go created online with Bitbucket

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"example-app/controllers"
+	"example-app/models"
+	"fmt"
+
+)
+
+
+
+func main() {
+
+	// Configure the mongodm connection 
+	dbConfig := &models.Config{
+		DatabaseHosts:[]string{"mongo:27017"},
+		DatabaseName:controllers.DBName,
+	}
+
+	// Connect and check for error
+	db, err := models.Connect(dbConfig)
+	if err != nil {
+		fmt.Println("Failed to establish connection to Mongo server:", err)
+	} else {
+		controllers.Database = db
+	}
+
+	uc := controllers.NewUserController(db)
+	r := gin.New()
+	// enable Logging
+	r.Use(gin.Logger())
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("/user",uc.Create)
+		v1.GET("/user",uc.UsersList)
+		v1.GET("/user/:id",uc.GetUser)
+		v1.PUT("/user/:id", uc.UpdateUser)
+	}
+
+	r.Run()
+}
+
+
